5fb9e093281465d324ebb4eb8349affcff7e48b9
goldfest <vano00189@mail.ru>
2025-10-05 15:06:58 +0400

    ready

---
+++ src/BankAccount.java
@@ -0,0 +1,112 @@
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Random;
+
+public class BankAccount {
+    //поля класса
+    private String ownerName;
+    private int balance;
+    private LocalDateTime openingDate;
+    private boolean isBlocked;
+    private String accountNumber;
+
+    //конструктор
+    public BankAccount(String ownerName) {
+        this.ownerName = ownerName;
+        this.balance = 0; //начальный баланс
+        this.openingDate = LocalDateTime.now(); //текущая дата
+        this.isBlocked = false; //счет не заблокирован по умолчанию
+        this.accountNumber = generateAccountNumber(); //генерация номера счета
+    }
+
+    //генерация номера счета
+    private String generateAccountNumber() {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
+    }
+
+    //пополнение счета
+    public boolean deposit(int amount) {
+        if (isBlocked || amount <= 0) {
+            return false;
+        }
+        balance += amount;
+        return true;
+    }
+
+    //снятие денег
+    public boolean withdraw(int amount) {
+        if (isBlocked || amount <= 0 || amount > balance) {
+            return false;
+        }
+        balance -= amount;
+        return true;
+    }
+
+    //перевод денег на другой счет
+    public boolean transfer(BankAccount otherAccount, int amount) {
+        if (isBlocked || otherAccount.isBlocked || amount <= 0 || amount > balance) {
+            return false;
+        }
+        this.balance -= amount;
+        otherAccount.balance += amount;
+        return true;
+    }
+
+    //геттеры и сеттеры
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public LocalDateTime getOpeningDate() {
+        return openingDate;
+    }
+
+    public boolean isBlocked() {
+        return isBlocked;
+    }
+
+    public void setBlocked(boolean blocked) {
+        isBlocked = blocked;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "ownerName='" + ownerName + '\'' +
+                ", balance=" + balance +
+                ", openingDate=" + openingDate +
+                ", isBlocked=" + isBlocked +
+                ", accountNumber='" + accountNumber + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BankAccount that = (BankAccount) o;
+        return Objects.equals(accountNumber, that.accountNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountNumber);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BankAccount account1 = new BankAccount("Иван Лобашов");
+        BankAccount account2 = new BankAccount("Степан Афанасьев");
+
+        System.out.println("Создание счетов");
+        System.out.println(account1);
+        System.out.println(account2);
+
+        System.out.println("\nПополнение счета");
+        boolean depositResult = account1.deposit(1000);
+        System.out.println("Пополнение на 1000: " + depositResult);
+        System.out.println("Баланс account1: " + account1.getBalance());
+
+        System.out.println("\nСнятие денег");
+        boolean withdrawResult = account1.withdraw(300);
+        System.out.println("Снятие 300: " + withdrawResult);
+        System.out.println("Баланс account1: " + account1.getBalance());
+
+        System.out.println("\nПеревод денег");
+        boolean transferResult = account1.transfer(account2, 200);
+        System.out.println("Перевод 200 на account2: " + transferResult);
+        System.out.println("Баланс account1: " + account1.getBalance());
+        System.out.println("Баланс account2: " + account2.getBalance());
+
+        System.out.println("\nТест некорректных операций");
+        //пробуем снять больше
+        boolean invalidWithdraw = account1.withdraw(1000);
+        System.out.println("Снятие 1000 (недостаточно средств): " + invalidWithdraw);
+
+        account2.setBlocked(true);
+        boolean transferToBlocked = account1.transfer(account2, 100);
+        System.out.println("Перевод на заблокированный счет: " + transferToBlocked);
+
+        System.out.println("\nТест equals");
+        BankAccount account3 = new BankAccount("Иван Лобашов");
+        System.out.println("account1 equals account2: " + account1.equals(account2));
+        System.out.println("account1 equals account3: " + account1.equals(account3));
+    }
+}
